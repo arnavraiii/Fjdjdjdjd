7562f96f3c549abad150fbe2a9999037734225c8
arnavraiii <ddosddosmam@gmail.com>
2024-12-30 19:49:31 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @MrRanDom8
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7907155504:AAE_KlRS9FM7jc5eEf0zaquVWQ-vzqTzmcQ")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("a4x_Crypto")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5047224084")
+
+
+
+
+  
